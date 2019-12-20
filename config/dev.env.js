adaeccc315a208ae8ed981ae3d118e7cff9e63b3
hupeng <guchengwuyue@163.com>
2019-12-20 15:46:08 +0800

    update env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://localhost:8000"'
-  // BASE_API: '"https://api.auauz.net"'
+  //BASE_API: '"https://yshopapi.dayouqiantu.cn"'
 })
