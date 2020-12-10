8563d41795ac12c7b97befe587eb01cc481fde94
朱耘稷 <18026623439@163.com>
2020-12-10 12:32:24 +0800

    公众号新增公众号二维码,新增商户端订单通知绑定

--- src/utils/index.js
+++ src/utils/index.js
@@ -419,3 +419,16 @@ export function downloadFile(obj, name, suffix) {
   link.click()
   document.body.removeChild(link)
 }
+
+export function RandomNumber() {
+  var min = 5; var max = 36
+  var randomStr = ''
+  var range = (max ? Math.round(Math.random() * (max - min)) + min : min)
+  var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+  for (var i = 0; i < range; i++) {
+    var index = Math.round(Math.random() * (arr.length - 1))
+    randomStr += arr[index]
+  }
+  return randomStr
+}
