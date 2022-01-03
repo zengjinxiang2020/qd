a6161f1511b9d5eec1fa97b588e33fed38b68c16
taozi <9108791@qq.com>
2022-01-03 16:45:41 +0800

    时间修改，Happy new year

--- src/settings.js
+++ src/settings.js
@@ -42,7 +42,7 @@ module.exports = {
   /**
    * 底部文字，支持html语法
    */
-  footerTxt: '© 2019-2021 YSHOP <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">All Rights Reserved</a>',
+  footerTxt: '© 2018-2022 YSHOP <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">All Rights Reserved</a>',
   /**
    * 备案号
    */
